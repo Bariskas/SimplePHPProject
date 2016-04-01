ac4d2626d76277b00088019e75948df451437ace
Bariskas <Bariskas@gmail.com>
2016-04-01 18:32:41 +0400

    Hello Iterator
    Iterator added

--- index.php
+++ index.php
@@ -1,2 +1,47 @@
 <?
-    echo 'hello world';
+    echo 'hello world <br/>';
+    
+    class MyIterator implements Iterator
+    {
+        private $iterArr = array();
+        public function __construct($array)
+        {
+            if (is_array($array)) 
+            {
+                $this->iterArr = $array;
+            }
+        }
+        public function rewind()
+        {
+            reset($this->iterArr);
+        }
+        public function current()
+        {
+            $currentEl = current($this->iterArr);
+            return $currentEl;
+        }
+        public function key() 
+        {
+            $key = key($this->iterArr);
+            return $key;
+        }
+        public function next() 
+        {
+            $nextEl = next($this->iterArr);
+            return $nextEl;
+        }
+        public function valid()
+        {
+            $key = key($this->iterArr);
+            $isValid = ($key !== NULL && $key !== FALSE);
+            return $isValid;
+        }
+    }
+    
+    $helloArray = array("Hello"," ","world","!");
+    $iterator = new myIterator($helloArray);
+    
+    foreach ($iterator as $word)
+    {
+        echo $word;
+    }
